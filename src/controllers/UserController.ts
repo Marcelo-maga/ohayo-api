6e7bfe4795fd5c94b73db59d09c6b4a379e4c204
Marcelo Magalhães <marcelo.magalhaes13@etec.sp.gov.br>
2021-07-22 12:43:46 -0300

    Login user com jwt

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,36 +1,85 @@
 import { client } from '../prisma/client'
-import { hash } from 'bcrypt'
+import { hash, compare } from 'bcrypt'
 import { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
 
 class UserController {
-  async newUser (request: Request, response: Response) {
+  async store (request: Request, response: Response) {
     const { email, password } = request.body
 
-    const emailExists = client.user.findUnique({
-      where: email
-    })
+    const emailExists = await client.user.findFirst({ where: { email } })
 
     if (emailExists) {
       response.statusCode = 400
       response.json({
         error: 'Email já cadastrado!'
       })
+      return
     } try {
-      const passwordHash = await hash(password, 8)
-      const user = await client.user.create({
+      const passCrypt = await hash(password, 8)
+      await client.user.create({
         data: {
           email,
-          password: passwordHash
+          password: passCrypt
         }
       })
-      return user
+      response.statusCode = 201
+      response.send('Conta criada com sucesso!')
     } catch (error) {
       response.statusCode = 400
+      response.send('Deu bo')
+    }
+  }
+
+  async login (request: Request, response: Response) {
+    const { email, password } = request.body
+
+    // verifica se o usuario existe
+    const user = await client.user.findFirst({ where: { email } })
+    if (!user) {
+      response.statusCode = 400
       response.json({
-        error: 'Não conseguimos criar sua conta!'
+        error: 'Email ou senha incorreta'
       })
+      return
+    } try {
+      // verifica a senha
+      const passwordMatch = await compare(password, user.password)
+      if (!passwordMatch) {
+        response.statusCode = 400
+        response.json({
+          error: 'Email ou senha incorreta'
+        })
+      }
+      // gera JWT
+      if (passwordMatch) {
+        jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET,
+          { expiresIn: '20s' }, (error, token) => {
+            if (error) {
+              response.statusCode = 400
+              response.json({
+                error: 'Erro ao autenticar!'
+              })
+              return false
+            } else {
+              response.statusCode = 200
+              response.json({
+                message: 'Sucesso!',
+                token
+              })
+            }
+          })
+      } else {
+        response.statusCode = 404
+        response.json({
+          msg: 'E-mail ou senha incorretos'
+        })
+      }
+    } catch (error) {
+      response.statusCode = 400
+      response.send('Deu bo')
+      console.log(error)
     }
   }
 }
-
 module.exports = new UserController()
--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ router.get('/', (request, response) => {
   response.send('Oi, você vem sempre aqui?')
 })
 
-router.post('/users', UserController.newUser)
+router.post('/users', UserController.store)
+router.post('/login', UserController.login)
 
 export { router }
