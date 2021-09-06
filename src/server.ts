831e07e25cbdcb379ed289ec736ac464e6a02e86
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-09-06 12:35:52 -0300

    Cors config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { router } from './routes'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import env from './config/env'
 
 const app = express()
@@ -9,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(router)
+app.use(cors({ origin: 'http://localhost:3000' }))
 
 app.listen(env.port, () => {
   console.log(`http://localhost:${env.port}`)
