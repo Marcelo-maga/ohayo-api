a3cc6f68c9ebc65b52b09ad15813335bcc053219
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-09-07 09:27:27 -0300

    Cors config, e eu não aguneto mais

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*')
   app.use(cors())
   next()
 })
