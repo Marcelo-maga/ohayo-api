dbef7c1909179a670ff34c3d4fc112f81fe2e680
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-09-20 15:54:08 -0300

    teste controller

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,7 +1,19 @@
+import { Request, Response } from 'express'
+// import { client } from 'src/prisma/client'
+import env from '@config/env'
+import { verify } from 'jsonwebtoken'
 
 class ProjectController {
-  async createProject () {
+  async createProject (request: Request, response: Response, token) {
+    // const { projectName, projectDesc } = request.body
 
+    const getUserEmail = verify(token, env.JWT_SECRET)
+
+    console.log(getUserEmail.email)
+
+    // const user = await client.user.findFirst({
+    //   where: { email }
+    // })
   }
 }
 
