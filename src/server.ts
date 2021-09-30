c42bfd8df367643cc6b68be09c29b872ea83eb36
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-09-30 09:12:03 -0300

    Ajsutes

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 
 import env from './config/env'
-import { router } from './routes'
-import { catchErros } from './middlewares/cacthErros'
+import router from './routes'
+import catchErros from './middlewares/cacthErros'
 
 const app = express()
 
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(cors())
-app.use(router)
 
+app.use(router)
 app.use(catchErros)
 
 app.listen(env.port, () => {
