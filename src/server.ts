e686e89cbeb8bcc0447a961a7b1b79ed4343336b
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-09-30 09:05:29 -0300

    middleware para captar erros

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import 'express-async-errors'
-import express, { Request, Response, NextFunction } from 'express'
+import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
 import env from './config/env'
 import { router } from './routes'
+import { catchErros } from './middlewares/cacthErros'
 
 const app = express()
 
@@ -14,14 +15,7 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(router)
 
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    return response.json({
-      status: 'Error',
-      message: error.message
-    })
-  }
-)
+app.use(catchErros)
 
 app.listen(env.port, () => {
   console.log(`http://localhost:${env.port}`)
