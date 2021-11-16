65f6b7f017ae840da7c8632f4bdf485114ef04bc
Marcelo <marcelo.maga2550@gmail.com>
2021-11-15 22:20:48 -0300

    Novas rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express'
 
-import isAuthenticate from './middlewares/isAuthenticate'
+import { isAuthenticate } from './middlewares/isAuthenticate'
 
 import { AuthenticateUserController } from './useCase/authenticateUser/AuthenticateUserController'
 import { CreateProjectController } from './useCase/createProject/CreateProjectController'
 import { CreateUserController } from './useCase/createUser/CreateUserController'
+import { GetAllProjectsUserController } from './useCase/getAllProjectsUser/getAllProjectsUserController'
+import { GetProjectController } from './useCase/getProject/getProjectController'
 
 // controllers
 const createUserController = new CreateUserController()
 const authUserController = new AuthenticateUserController()
 const createProject = new CreateProjectController()
+const getAllProjects = new GetAllProjectsUserController()
+const getProject = new GetProjectController()
 
 const router = Router()
 
@@ -21,5 +25,7 @@ router.post('/register', createUserController.handle)// rota de cadastro do usua
 router.post('/login', authUserController.handle)// rota de login do usuario
 
 router.post('/newProject', isAuthenticate, createProject.handle)// Rota de criação de projeto
+router.get('/getAllProjects', isAuthenticate, getAllProjects.handle)
+router.get('/getProject/:id', isAuthenticate, getProject.handle)
 
 export default router
